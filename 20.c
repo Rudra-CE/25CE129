a75a74a23d429cff3c8fa5231189d413ee8e9817
Rudra-CE <rudravaghasiya.ce@gmail.com>
2025-09-04 16:13:59 +0530

    Add files via upload

---
+++ 20.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,asum=0,esum=0,missing ;
+    printf("enter your array size :");
+    scanf("%d",&n);
+
+    int arr[n-1];
+
+    for(i=0; i<n-1; i++)
+    {
+
+
+        printf("the array[%d]= ",i);
+        scanf("%d",&arr[i]);
+        asum=asum+arr[i];
+    }
+    esum=n*(n+1)/2;
+
+    missing=esum-asum;
+    printf("missing value is :%d",missing);
+
+}
+
+
+
+
