a91f55710b36726a90e06f47e022779e5f5320fd
Rudra-CE <rudravaghasiya.ce@gmail.com>
2025-11-19 22:13:46 +0530

    Update and rename 16.c to P16.c

--- P16.c
+++ P16.c
@@ -43,7 +43,10 @@ int main()
             break;
         }
     }
-
+printf("Name : Vaghasiya Rudra Hiteshbhai\n");
+    printf("Id   : 25CE129\n");
+    printf("Batch: C-2\n");
     return 0;
 }
+
 
