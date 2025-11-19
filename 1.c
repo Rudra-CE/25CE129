94c3b3d043022b1627717e36b167342c32298a74
Rudra-CE <rudravaghasiya.ce@gmail.com>
2025-11-19 20:39:42 +0530

    Modify program to include Hello World message

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+printf("Hello World\n");
+printf("NAME : my name is rudra vaghasiya\n");
+printf("DEPARTMENT : COMPUTER ENGEENERING\n");
+getch();
+
+return 0;
+}
+
