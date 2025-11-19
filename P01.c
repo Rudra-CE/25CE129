7bebeee6a23cf22679bfc059b43bee7211dc3fa2
Rudra-CE <rudravaghasiya.ce@gmail.com>
2025-11-19 22:04:35 +0530

    Include ID in output of P01.c
    Added ID output to the program.

--- P01.c
+++ P01.c
@@ -5,9 +5,11 @@ int main()
 clrscr();
 printf("Hello World\n");
 printf("NAME : my name is rudra vaghasiya\n");
+printf("ID : 25CE129\n")
 printf("DEPARTMENT : COMPUTER ENGEENERING\n");
 getch();
 
 return 0;
 }
+
 
