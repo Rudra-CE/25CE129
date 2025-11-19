51fe99c3135fbada1d2d366b190b099a4518061f
Rudra-CE <rudravaghasiya.ce@gmail.com>
2025-11-20 01:17:50 +0530

    Update and rename 22.c to P22.c

--- P22.c
+++ P22.c
@@ -1,55 +1,46 @@
 #include<stdio.h>
 int main()
 {
-    char arry[5][10];
-    int row,seat,rseat;
-    int i,j;
-    for(i=0; i<5; i++)
+    char seating[5][10];
+    int i,j,reserved_seats,row,seat;
+    for(i=0;i<5;i++)
     {
-        for(j=0; j<10; j++)
+        for(j=0;j<10;j++)
         {
-            arry[i][j]='0';
+            seating[i][j]='0';
         }
     }
-    printf("enter the number of seat you want to reserved:");
-    scanf("%d",&rseat);
-    for(i=1;i<=rseat;i++)
+    printf("Welcome to Theater Seating Arrangement\n");
+    printf("Enter the number of reserved seat: ");
+    scanf("%d",&reserved_seats);
+
+    for(i=1;i<=reserved_seats;i++)
     {
-        printf("enter row and seat number for reserved seat :%d",i);
+        printf("Enter the row and seat number for reserved seat %d(eg. 2 5):",i);
         scanf("%d %d",&row,&seat);
-        if(row>0 && row<=5 && seat>0 && seat<=10)
+        if(row>1 && row<=5 && seat>1 && seat<=10)
         {
-            arry[row-1][seat-1] ='x';
-       }
-
-       else
-       {
-           printf("invalid row and seat number.please enter a valid one\n");
-           i--;
-       }
+            seating[row-1] [seat-1]='X';
+        }
+        else
+        {
+            printf("Invalid row or seat number.Please enter a valid one\n");
+            i--;
+        }
     }
-printf("seating chars : \n");
-
-
-    for(i=0; i<5; i++)
+    printf("\nSeating Chart:\n");
+    for(i=0;i<5;i++)
     {
-
-         printf("row %d :",i+1);
-                for(j=0; j<10; j++)
-
+        printf("Row %d:",i+1);
+        for(j=0;j<10;j++)
         {
-            printf("%c ",arry[i][j]);
+                printf("%3c",seating[i][j]);
         }
         printf("\n");
     }
-    printf("Name : Vaghasiya Rudra Hiteshbhai\n");
+printf("Name : Vaghasiya Rudra Hiteshbhai\n");
     printf("Id   : 25CE129\n");
     printf("Batch: C-2\n");
 return 0;
+    return 0;
 }
-
-
-
-
-
-
