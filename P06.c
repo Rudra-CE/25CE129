d6a80e5b951616956648bad1ba749ec599a42718
Rudra-CE <rudravaghasiya.ce@gmail.com>
2025-11-19 22:08:31 +0530

    Update and rename 6.c to P06.c

--- P06.c
+++ P06.c
@@ -11,7 +11,11 @@ int main()
     Fahrenheit=(Celsius*9/5)+32;
 
     printf("Temperature in Fahrenheit:%lf" ,Fahrenheit );
+    printf("Name : Vaghasiya Rudra Hiteshbhai\n");
+    printf("Id   : 25CE129\n");
+    printf("Batch: C-2\n");
 }
+
 
 
 
