8a9a9fff1788568491c96dc432da23f7beeda119
Rudra-CE <rudravaghasiya.ce@gmail.com>
2025-11-19 23:52:11 +0530

    Update and rename p01.c to P01.c

--- P01.c
+++ P01.c
@@ -5,11 +5,12 @@ int main()
 clrscr();
 printf("Hello World\n");
 printf("NAME : my name is rudra vaghasiya\n");
-printf("ID : 25CE129\n")
+printf("ID : 25CE129\n");
 printf("DEPARTMENT : COMPUTER ENGEENERING\n");
 getch();
 
 return 0;
 }
+
 
 
